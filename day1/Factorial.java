cee9c2b79dce9b65d8aa7d21c8dfa8712b1da154
Sadhurya30 <132685899+Sadhurya30@users.noreply.github.com>
2023-05-17 21:34:05 +0530

    Add files via upload

---
+++ day1/Factorial.java
@@ -0,0 +1,17 @@
+package week2.day1;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+        int range = 4;
+        int sum =1;
+		for(int i =1;i<=range;i++)
+		{
+			sum =sum *i;
+		}
+		System.out.println(sum);
+	}
+
+}
